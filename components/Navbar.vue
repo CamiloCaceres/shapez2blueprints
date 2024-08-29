a7fbfd1fa615221564ef2d15b3c2a0a8346eee9a
Camilo Caceres <camiloac97@gmail.com>
2024-08-29 16:02:31 -0500

    feat: add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="bg-gray-100 dark:bg-gray-800 sticky top-0 z-50 shadow-md">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex space-x-16 items-center">
+
+            <div class="ml-4">
+              <NuxtLink
+                class="text-gray-800 dark:text-gray-200 text-2xl font-bold"
+                to="/"
+              >
+                🔹 Shapez2Blueprints
+              </NuxtLink>
+            </div>
+
+          </div>
+          <div class="flex items-center space-x-4">
+            <UButton @click="signOut"v-if="currentUser">Logout</UButton>
+            <UButton  v-else to="/login">Login</UButton>
+  
+            
+          </div>
+        </div>
+      </div>
+
+    </nav>
+  </template>
+  
+  <script setup lang="ts">
+  import { pb, currentUser  } from '@/utils/pocketbase'
+
+  function signOut() {
+    pb.authStore.clear()
+  }
+
+  </script>
