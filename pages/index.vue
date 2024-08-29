751e1575047d1d8653049490f528b53a08052645
Camilo Caceres <camiloac97@gmail.com>
2024-08-29 16:02:13 -0500

    refactor: add pages

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://shapez2blueprints.pockethost.io');
+pb.autoCancellation(false);
+
+
+// fetch a paginated records list
+const resultList = await pb.collection('blueprints').getList(1, 50, {
+    filter: 'created >= "2022-01-01 00:00:00"',
+});
+
+</script>
+
+<template>
+<pre>
+    {{ resultList }}
+    <Login />
+</pre></template>
