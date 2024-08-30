ccd073cafdb1560f93dd0e368371634267cc4a3c
Camilo Caceres <camiloac97@gmail.com>
2024-08-30 11:05:45 -0500

    feat: add dynamic fetch blueprints

---
+++ pages/blueprints/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { pb } from "@/utils/pocketbase";
+
+const route = useRoute()
+
+const resultList = ref(null);
+
+
+onMounted(async () => {
+  try {
+    // fetch a paginated records list
+    resultList.value = await pb.collection("blueprints").getOne(route.params.id);
+  } catch (error) {
+    console.error("Error fetching blueprints:", error);
+  }
+});
+</script>
+
+<template>
+    <h1>{{ resultList }}</h1>
+</template>
