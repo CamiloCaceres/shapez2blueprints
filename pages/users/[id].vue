54f9f561769d7cd249c3f8468e1192c59cb4a47c
Camilo Caceres <camiloac97@gmail.com>
2024-09-01 02:14:21 -0500

    feat: add dunamic route for user blueprints

---
+++ pages/users/[id].vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { pb } from "@/utils/pocketbase";
+
+const route = useRoute();
+
+const blueprints = ref(null);
+
+const getImageUrl = (item) => {
+  if (item && item.image) {
+    return pb.files.getUrl(item, item.image, { thumb: "800x0" });
+  }
+  return "";
+};
+
+onMounted(async () => {
+  try {
+    // fetch a paginated records list
+    blueprints.value = await pb.collection("blueprints").getList(1, 50, {
+        filter: `author = '${route.params.id}'`
+    });
+  } catch (error) {
+    console.error("Error fetching blueprints:", error);
+  }
+});
+</script>
+<template>
+    <h1>    hi</h1>{{ route.params.id }}
+    <pre>{{ blueprints }}</pre>
+</template>
